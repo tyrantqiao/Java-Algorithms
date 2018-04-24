be94ab35511607a3af39bf03750e0c8c532cdb36
Administrator <tyrantqiao@icloud.com>
2018-04-24 08:19:50 +0800

    learn hashMap source code

---
+++ src/main/java/leetcode/TwoSum.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Created By TyrantQiao on 2018/4/24
+ *
+ * @version 0.0.1
+ * Description: two number sum
+ * blog https://tyrantqiao.github.io/Blog
+ * @see <a href="https://github.com/tyrantqiao">qiao's github</a>
+ */
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int supplement = target - nums[i];
+			if (map.containsKey(supplement) && map.get(supplement) != i) {
+				return new int[]{i, map.get(supplement)};
+			}
+			map.put(nums[i], i);
+		}
+		return null;
+	}
+}
