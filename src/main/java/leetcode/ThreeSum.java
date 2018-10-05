43594529a3d17c0a905d08f0115fc16e7ee98b3a
Administrator <tyrantqiao@icloud.com>
2018-10-05 17:03:14 +0800

    update (leetcode): ThreeSum的注释和重新理解

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -15,6 +15,9 @@ import java.util.List;
 public class ThreeSum {
 	/**
 	 * 思路大概是我们通过i，将数组划分为一个个小的two sum查找，同时也对重复元素进行处理。o(n^2)
+	 * 同时由于我们对数组排序了，所以当我们迭代时，发现结果大于0时，可以通过降低hi来逼近0值，反之升高lo值
+	 * a b c d e
+	 * 迭代时通过lo和hi指针进行，lo=target+1,hi=size-1所以当target到倒数第三个元素时就迭代结束了【notIterate=2】
 	 *
 	 * @param nums
 	 * @return
@@ -22,22 +25,27 @@ public class ThreeSum {
 	public static List<List<Integer>> threeSum(int[] nums) {
 		List<List<Integer>> result = new LinkedList<>();
 		Arrays.sort(nums);
-		int n = nums.length;
+		int size = nums.length;
 		int notIterate = 2;
-		for (int i = 0; i + notIterate < n; i++) {
+		for (int i = 0; i + notIterate < size; i++) {
+			//若元素一样，则通过continue使得i往后迭代
 			if (i > 0 && nums[i] == nums[i - 1]) {
 				continue;
 			}
-			int lo = i + 1, hi = n - 1;
+			//两个指针，注意此处lo=i+1
+			int lo = i + 1, hi = size - 1;
 			int target = nums[i];
 			while (lo < hi) {
 				if (nums[lo] + nums[hi] + target == 0) {
 					result.add(Arrays.asList(nums[lo], nums[hi], target));
 
+					//若元素相等，指针移动
 					while (lo < hi && nums[lo] == nums[lo + 1]) { lo++;}
 					while (lo < hi && nums[hi] == nums[hi - 1]) { hi--;}
+					//而因为添加了结果，所以指针得移动
 					lo++;
 					hi--;
+					//此处为！！核心处！！当加的结果为正值时，hi下降，反之lo上升
 				} else if (nums[lo] + nums[hi] + target > 0) {hi--;} else {lo++;}
 			}
 
