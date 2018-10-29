f7c093e50c81b8faf207b592af560f75f0e14ecc
tyrantqiao <tyrantqiao@icloud.com>
2018-10-29 23:08:41 +0800

    update (leetcode): RemoveElement算个取巧的方法

--- src/main/java/leetcode/RemoveElement.java
+++ src/main/java/leetcode/RemoveElement.java
@@ -25,7 +25,7 @@ public class RemoveElement {
         int notEqualIndex = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[notEqualIndex] = val;
+                nums[notEqualIndex] = nums[i];
                 notEqualIndex++;
             }
         }
