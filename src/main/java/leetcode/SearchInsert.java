c7ba215b1c49c82c711542e99992bf0ea24fb065
tyrantqiao <tyrantqiao@icloud.com>
2018-11-07 22:49:08 +0800

    add (leetcode): searchInsert二分判断得结果

---
+++ src/main/java/leetcode/SearchInsert.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * @author tyrantqiao
+ * date: 2018/11/7
+ * blog: tyrantqiao.com
+ * contact: tyrantqiao@icloud.com
+ */
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int high = nums.length - 1, low = 0;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        SearchInsert searchInsert = new SearchInsert();
+        System.out.println(searchInsert.searchInsert(new int[]{1, 2, 3, 6, 8}, 4));
+    }
+}
