254ff703205851d42709867961dd55ffc4a4c405
tyrantqiao <tyrantqiao@icloud.com>
2018-12-06 17:23:01 +0800

    add (leetcode): 注释以及一些题目答案等等

---
+++ src/main/java/interview/ShenZhouInterview.java
@@ -0,0 +1,16 @@
+package interview;
+
+/**
+ * @author tyrantqiao
+ * date: 2018/11/22
+ * blog: tyrantqiao.com
+ * contact: tyrantqiao@icloud.com
+ */
+public class ShenZhouInterview {
+
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("1:"+  ++i);
+    }
+}
