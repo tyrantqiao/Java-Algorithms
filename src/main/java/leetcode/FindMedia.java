861bfede6499fd313b12587112188dc00443881d
tyrantqiao <tyrantqiao@icloud.com>
2019-01-15 20:17:06 +0800

    add (leetcode): FindMedia方法解释

--- src/main/java/leetcode/FindMedia.java
+++ src/main/java/leetcode/FindMedia.java
@@ -7,20 +7,30 @@ import java.util.Queue;
  * @author tyrantqiao
  */
 public class FindMedia {
-	private Queue<Long> small = new PriorityQueue();
-	private Queue<Long> big = new PriorityQueue();
+    private Queue<Long> small = new PriorityQueue();
+    private Queue<Long> big = new PriorityQueue();
 
-	public void addNum(int num) {
-		big.add((long) num);
-		small.add(-big.poll());
-		if (big.size() < small.size()) {
-			big.add(-small.poll());
-		}
-	}
+    /**
+     * 添加数字
+     *
+     * @param num
+     */
+    public void addNum(int num) {
+        big.add((long) num);
+        small.add(-big.poll());
+        if (big.size() < small.size()) {
+            big.add(-small.poll());
+        }
+    }
 
-	public double findMedian() {
-		return big.size() > small.size()
-				? big.peek()
-				: (big.peek() - small.peek()) / 2.0;
-	}
+    /**
+     * 查找中间数，获得队列的头部
+     *
+     * @return
+     */
+    public double findMedian() {
+        return big.size() > small.size()
+                ? big.peek()
+                : (big.peek() - small.peek()) / 2.0;
+    }
 }
