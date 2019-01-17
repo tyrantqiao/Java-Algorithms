2b0bfff75cc0ef147a16d1ed82f67dd0c179310a
tyrantqiao <tyrantqiao@icloud.com>
2019-01-17 22:54:37 +0800

    add (leetcode): 将listNode转为arrayList形式

--- src/main/java/leetcode/ListNodeQuestions.java
+++ src/main/java/leetcode/ListNodeQuestions.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author tyrantqiao
@@ -31,6 +30,7 @@ public class ListNodeQuestions {
 
     /**
      * 答案做法O(1)的空间消耗量，时间上O(n)，双节点快速遍历，左节点负责遍历完整一周，右节点快速排查且尽量与左节点见面
+     *
      * @param head
      * @return
      */
@@ -48,5 +48,33 @@ public class ListNodeQuestions {
             right = right.next.next;
         }
         return true;
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+
+        if (root == null) {
+            return result;
+        }
+
+        // 将root作为tail节点加入，若为首时则加做首first
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            // DFS
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                subList.add(queue.poll().val);
+            }
+            result.add(0, subList);
+        }
+        return result;
     }
 }
