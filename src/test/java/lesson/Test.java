6e64a1b2675e1a658dd01be6da53b4ef156f0874
tyrantqiao <tyrantqiao@gmail.com>
2021-04-17 16:15:26 +0800

    测试题目用的

---
+++ src/test/java/lesson/Test.java
@@ -0,0 +1,18 @@
+package lesson;
+
+/**
+ * 考试$
+ * <p>
+ * 创建时间： 2021/4/4 12:58
+ * 博客地址： <a href="https://tyrantqiao.com">详情戳我(╯‵□′)╯︵┻━┻)</a>
+ *
+ * @author tyrantqiao
+ * @version 1.0
+ **/
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println(String.format("%.1f",3.35f));
+        System.out.println(String.format("%.1f",3.35));
+    }
+}
